376d9b8a24dcb076741163cccc9f64b576dbbefa
Ji Smith <jihyunjung0219@gmail.com>
2020-10-20 21:33:27 -0500

    seeds.sql created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, dirnk_type, total, set_menu) VALUES ('cheese burger', 'coke', 8.50, True);
+INSERT INTO burgers (burger_name, dirnk_type, total, set_menu) VALUES ('veggie burger', 'pepsi', 7.50, True);
+INSERT INTO burgers (burger_name, dirnk_type, total, set_menu) VALUES ('chicekn burger', 'juice', 8.00, False);
+
