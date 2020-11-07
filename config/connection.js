f51c09b9cb00e355c5dcf1439873ceed43296cd5
Ji Smith <jihyunjung0219@gmail.com>
2020-11-07 11:25:14 -0600

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -13,22 +13,18 @@ if (process.env.JAWSDB_URL){
     });
 };
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
 
-//Connect to the database
-connection.connect((err) => {
-    if (err) {
-        console.error(`Error connecting to DB: ${err.message}`);
-        return;
-    }
-    console.log(`Connected to DB with connection ID ${connection.threadId}`);
-});
+connection.connect();
+
+
+// //Connect to the database
+// connection.connect((err) => {
+//     if (err) {
+//         console.error(`Error connecting to DB: ${err.message}`);
+//         return;
+//     }
+//     console.log(`Connected to DB with connection ID ${connection.threadId}`);
+// });
 
 
 //???//
